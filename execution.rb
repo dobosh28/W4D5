cb1e77e2328cf1a055d0940f04d7cf97786acd8e
Yosyp Dobosh <joseph.dobosh@gmail.com>
2022-08-12 15:25:00 -0400

    complete execution time differences/algorithms

--- execution.rb
+++ execution.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 def my_min_1(arr) # o(n ^2)
    sorted = false # c
    while !sorted # n
@@ -23,3 +25,41 @@ def my_min_2(arr) # o(n)
     min #c
 end
 
+def largest_contiguous_sub_sum_1(arr) #n^2
+    subarrs = []
+
+    (0..arr.length - 1).each do |i| #n
+        (i..arr.length - 1).each do |j| #n
+            subarrs << arr[i..j] #n
+        end
+    end
+
+    sums = subarrs.map { |subarr| subarr.sum }
+    sums.max
+end
+
+list_1 = [5, 3, -7]
+list_2 = [2, 3, -6, 7, -6, 7]
+list_3 = [-5, -1, -3]
+
+def largest_contiguous_sub_sum_2(arr) #O(n)
+    max = arr.first
+    current = arr.first
+
+    arr.drop(1).each do |num|
+    
+        if current < 0
+            current = 0
+        end
+
+        current += num
+
+        if current > max
+            max = current
+        end
+    end
+
+    max
+end
+
+p largest_contiguous_sub_sum_2(list_2)
